31acfe9578cf69322567555016693abcfdf9e81c
fitri-hy <noreply.orzpartners@gmail.com>
2025-12-04 15:52:22 +0700

    build: update vercel configuration and entrypoint

--- gateway/index.js
+++ gateway/index.js
@@ -1,10 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-
 import apiRoute from "./routes/api.js";
 import webRoute from "./routes/web.js";
 import { ok } from "./utils/response.js";
@@ -13,13 +11,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-
 app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
@@ -29,15 +24,6 @@ app.use((req, res, next) => {
 
 app.use("/data", apiRoute);
 app.use("/", webRoute);
-
-app.use((req, res, next) => {
-  res.status(404).send("Page not found");
-});
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`[GATEWAY] running on http://localhost:${PORT}`));
